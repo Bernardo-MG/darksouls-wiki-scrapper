9402aa06cededee7c1996332df8a8b3b90b142bc
Bernardo <programming@bernardomg.com>
2021-08-19 22:00:06 +0200

    Reduced scrappers

--- scrapper/name_list.py
+++ scrapper/name_list.py
@@ -20,83 +20,66 @@ class NameListScrapper(object):
         return list(map(lambda item: {'name': item.get_text()}, main_list))
 
 
-class BaseNameListScrapper(CsvScrapper):
-    """
-    Scrapper for name list pages. Will find all the names in a page and store them in a list.
-    """
-
-    def __init__(self, root_url, list_page, output_file):
-        super(BaseNameListScrapper, self).__init__(root_url + list_page, output_file, ['name'])
-        self.inner_parser = NameListScrapper(self._extract_links)
-
-    @abstractmethod
-    def _extract_links(self, dom):
-        """
-        Extracts the list of names from the DOM.
-
-        Returns
-        -------
-        list
-            a list of text elements
-        """
-        pass
-
-
-class ArmorSetScrapper(BaseNameListScrapper):
+class ArmorSetScrapper(CsvScrapper):
     """
     Armor set scrapper.
     """
 
-    def __init__(self, root):
-        super(ArmorSetScrapper, self).__init__(root, '/wiki/Armor_(Dark_Souls)', 'output/armor_sets.csv')
+    def __init__(self, root_url):
+        super(ArmorSetScrapper, self).__init__(root_url + '/wiki/Armor_(Dark_Souls)', 'output/armor_sets.csv', ['name'])
+        self.inner_parser = NameListScrapper(self._extract_links)
 
     def _extract_links(self, dom):
         return dom.select('table:nth-of-type(1) li a')
 
 
-class ShieldTypeScrapper(BaseNameListScrapper):
+class ShieldTypeScrapper(CsvScrapper):
     """
     Shield type scrapper.
     """
 
-    def __init__(self, root):
-        super(ShieldTypeScrapper, self).__init__(root, '/wiki/Shields', 'output/shield_types.csv')
+    def __init__(self, root_url):
+        super(ShieldTypeScrapper, self).__init__(root_url + '/wiki/Shields', 'output/shield_types.csv', ['name'])
+        self.inner_parser = NameListScrapper(self._extract_links)
 
     def _extract_links(self, dom):
         return dom.select('dt a')
 
 
-class WeaponTypeScrapper(BaseNameListScrapper):
+class WeaponTypeScrapper(CsvScrapper):
     """
     Weapon type scrapper.
     """
 
-    def __init__(self, root):
-        super(WeaponTypeScrapper, self).__init__(root, '/wiki/Weapon_Types_(Dark_Souls)', 'output/weapon_types.csv')
+    def __init__(self, root_url):
+        super(WeaponTypeScrapper, self).__init__(root_url + '/wiki/Weapon_Types_(Dark_Souls)', 'output/weapons.csv', ['name'])
+        self.inner_parser = NameListScrapper(self._extract_links)
 
     def _extract_links(self, dom):
         return dom.select('dt a')
 
 
-class UniqueWeaponScrapper(BaseNameListScrapper):
+class UniqueWeaponScrapper(CsvScrapper):
     """
     Unique weapon scrapper.
     """
 
-    def __init__(self, root):
-        super(UniqueWeaponScrapper, self).__init__(root, '/wiki/Category:Dark_Souls:_Unique_Weapons', 'output/unique_weapons.csv')
+    def __init__(self, root_url):
+        super(UniqueWeaponScrapper, self).__init__(root_url + '/wiki/Category:Dark_Souls:_Unique_Weapons', 'output/unique_weapons.csv', ['name'])
+        self.inner_parser = NameListScrapper(self._extract_links)
 
     def _extract_links(self, dom):
         return dom.select('ul.category-page__members-for-char li a.category-page__member-link')
 
 
-class EnemyScrapper(BaseNameListScrapper):
+class EnemyScrapper(CsvScrapper):
     """
     Enemy list scrapper.
     """
 
-    def __init__(self, root):
-        super(EnemyScrapper, self).__init__(root, '/wiki/Category:Dark_Souls:_Enemies', 'output/enemies.csv')
+    def __init__(self, root_url):
+        super(EnemyScrapper, self).__init__(root_url + '/wiki/Category:Dark_Souls:_Enemies', 'output/enemies.csv', ['name'])
+        self.inner_parser = NameListScrapper(self._extract_links)
 
     def _extract_links(self, dom):
         result = dom.select('li a.category-page__member-link')

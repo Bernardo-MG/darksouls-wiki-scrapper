0d5dd1ed295f0671599d6513422c039642335302
Bernardo <programming@bernardomg.com>
2022-05-05 17:57:50 +0200

    Extended weapon info

--- scrapper/item/weapon.py
+++ scrapper/item/weapon.py
@@ -46,6 +46,28 @@ class StatsScrapper(object):
         if attacks == '-':
             attacks = ''
 
+        # Requirements
+        strength = dom.select('div.page.has-right-rail aside td[data-source="str-req"]')
+        if len(strength) > 0:
+            strength = strength[0].get_text()
+        else:
+            strength = ''
+        dexterity = dom.select('div.page.has-right-rail aside td[data-source="dex-req"]')
+        if len(dexterity) > 0:
+            dexterity = dexterity[0].get_text()
+        else:
+            dexterity = ''
+        intelligence = dom.select('div.page.has-right-rail aside td[data-source="int-req"]')
+        if len(intelligence) > 0:
+            intelligence = intelligence[0].get_text()
+        else:
+            intelligence = ''
+        faith = dom.select('div.page.has-right-rail aside td[data-source="fth-req"]')
+        if len(faith) > 0:
+            faith = faith[0].get_text()
+        else:
+            faith = ''
+
         # Description
         description = dom.select('div.mainbg dd i')
         info = []
@@ -56,7 +78,8 @@ class StatsScrapper(object):
         description = '\\n'.join(info)
 
         return {'name': name, 'type': type_name, 'description': description, 'weight': weight, 'durability': durability,
-                'attacks': attacks}
+                'attacks': attacks, 'strength': strength, 'dexterity': dexterity, 'intelligence': intelligence,
+                'faith': faith}
 
 
 class WeaponDescriptionScrapper(CsvScrapper):
@@ -66,7 +89,8 @@ class WeaponDescriptionScrapper(CsvScrapper):
 
     def __init__(self, root_url):
         super(WeaponDescriptionScrapper, self).__init__(root_url + '/wiki/Weapons_(Dark_Souls)', 'output/weapons.csv',
-                                                        ['name', 'type', 'description', 'weight', 'durability', 'attacks'])
+                                                        ['name', 'type', 'description', 'weight', 'durability', 'attacks',
+                                                         'strength', 'dexterity', 'intelligence', 'faith'])
         self.inner_parser = ListScrapper(root_url, StatsScrapper(), lambda dom: self._extract_links(dom))
 
     def _extract_links(self, dom):

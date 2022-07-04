c3334b4688818468ea3dfe682ce60129d12a590f
Bernardo <programming@bernardomg.com>
2022-07-04 22:47:10 +0200

    Rejects invalid values

--- scrapper/item/talisman_attr_adjustment.py
+++ scrapper/item/talisman_attr_adjustment.py
@@ -32,7 +32,9 @@ class AdjustmentScrapper(object):
             values = dom.select('table.wiki_table td:nth-child(%d)' % (i + 1))
             values = list(map(lambda c: c.get_text(), values))
             for j in range(1, attr_count):
-                result.append({'name': name, 'faith': attr[j], 'adjustment': values[j]})
+                value = values[j]
+                if value != '-' and value != '':
+                    result.append({'name': name, 'faith': attr[j], 'adjustment': value})
 
         return result
 
